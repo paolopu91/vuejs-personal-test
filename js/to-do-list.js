6dbc4096956ccea682bd8767e90decd80c411694
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-13 15:55:06 +0200

    added html

---
+++ js/to-do-list.js
@@ -0,0 +1,20 @@
+new Vue({
+    el:"#app",
+    data:{
+        toDoList:[{
+            text: "How to study Vue.js?",
+            done: false
+        },
+        {
+            text: "Study with Vue.js",
+            done: false
+        },
+        {
+            text: "Suicide with Vue,js",
+            done: false 
+        }
+    ]
+        
+
+    }
+})
